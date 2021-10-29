6aae5da2abaab493e56cfd363302d10609381491
Sameony <iam2001sam@gmail.com>
2021-10-29 18:25:58 +0530

    feat: Gives tip and per person amount

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const inputBill = document.querySelector("#inputBill");
 const inputTip = document.querySelector("#inputTip");
 const inputCount = document.querySelector("#inputCount");
 const btnCheck = document.querySelector(".check");
+const tipOutput = document.querySelector(".tipOutput");
+const perPersonOutput = document.querySelector(".perPersonOutput");
+
 
 btnCheck.addEventListener("click",function checkOutPut(){
-    console.log(inputBill.value + inputTip.value + inputCount.value);
+    var totalTip = (inputBill.value*inputTip.value)/100;
+    totalTip=totalTip.toFixed(2);
+    var perPerson = (inputBill.value)/inputCount.value;
+    tipOutput.innerText=("Total tip = "+totalTip);
+    perPersonOutput.innerText=("Per person bill = "+perPerson);
 })
